76f541a1d6941a52e6a00e3f7a2c8253bffd32b3
masud rana <masudranaabc@gmail.com>
2022-09-21 09:28:53 +0600

    array push,pop

--- array.js
+++ array.js
@@ -1,12 +1,20 @@
-let name=["anis","afrin","sweety"];
-console.log(name.indexOf("afrin"))
-// console.log(name[2])
+// let name=["anis","afrin","sweety"];
+// console.log(name.indexOf("afrin"))
+// // console.log(name[2])
 
-let num=[1,23,4,5,6]
-// console.log(num)
-// console.log(num.indexOf(4))
-// console.log(num.indexOf(42))
-// console.log(num[3])
-// console.log(num[32])
-num[4]=25;
-// console.log(num)
+// let num=[1,23,4,5,6]
+// // console.log(num)
+// // console.log(num.indexOf(4))
+// // console.log(num.indexOf(42))
+// // console.log(num[3])
+// // console.log(num[32])
+// num[4]=25;
+// // console.log(num)
+// let numbers=[10,11,12,13]
+// // console.log(numbers)
+// numbers.push(20)
+// console.log(numbers)
+// // console.log(add1)
+// let removeItem=numbers.pop()
+// console.log(removeItem)
+// console.log(numbers)
